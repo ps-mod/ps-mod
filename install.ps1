9c4a58082737b466e1c0c1e108b3421ae0c4524b
Dombi Tibor <dombidav@gmail.com>
2021-03-04 08:49:24 +0100

    I don't have any more commit messages
    For the following generations: DO NOT use the vscode's included git tools

---
+++ install.ps1
@@ -0,0 +1,5 @@
+$path = "C:\tools"
+New-Item -Path $path -ItemType Directory -Force
+git clone https://github.com/ps-mod/ps-mod.git $path
+
+Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Microsoft.PowerShell_profile.ps1') -Value "`r`nImport-Module $path\ps-mod\load.ps1"
