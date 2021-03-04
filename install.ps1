00b9d5e1cad5f42019d25f078ed3811cdcb6a3eb
Dombi Tibor <dombidav@gmail.com>
2021-03-04 11:17:12 +0100

    I give up

--- install.ps1
+++ install.ps1
@@ -2,4 +2,4 @@ $path = "C:\tools\ps-mod"
 New-Item -Path $path -ItemType Directory -Force
 git clone https://github.com/ps-mod/ps-mod.git $path
 
-Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Microsoft.PowerShell_profile.ps1') -Value "`r`nImport-Module $path\ps-mod\load.ps1"
+Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Microsoft.PowerShell_profile.ps1') -Value "`r`nImport-Module $path\load.ps1"
