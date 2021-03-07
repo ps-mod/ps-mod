ce8cc40ce1d425809036f418d45051c66ba697db
Dombi Dávid <dombidav@gmail.com>
2021-03-07 21:35:39 +0100

    Added missing profiles to install.ps1

--- install.ps1
+++ install.ps1
@@ -3,9 +3,15 @@ $content = "`r`nImport-Module $path\load.ps1"
 New-Item -Path $path -ItemType Directory -Force
 git clone https://github.com/ps-mod/ps-mod.git $path
 
-Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1') -Value $content
-Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Microsoft.PowerShell_profile.ps1') -Value $content
-Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\PowerShellISE_profile.ps1') -Value $content
-Add-Content -Path ([Environment]::GetFolderPath("MyDocuments") + '\WindowsPowerShell\Profile.ps1') -Value $content
-Add-Content -Path ("$PsHome\Microsoft.PowerShell_profile.ps1") -Value $content
-Add-Content -Path ("$PsHome\Profile.ps1") -Value $content
+$profiles = (
+    $profile.CurrentUserAllHosts,
+    $profile.CurrentUserCurrentHost,
+    $profile.AllUsersAllHosts,
+    $profile.AllUsersCurrentHost
+)
+
+$profiles | ForEach-Object {
+    if(Test-Path $_){
+        Add-Content -Path ($_) -Value $content
+    }
+}
